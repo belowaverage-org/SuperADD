f45c1cb378bd90f7bb29774deb2240f808f988a2
Dylan Bickerstaff <dylan@belowaverage.org>
2018-12-29 00:57:58 -0500

    Final 0.0.0.1

--- SuperADD/Properties/AssemblyInfo.cs
+++ SuperADD/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApp2")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApp2")]
+[assembly: AssemblyTitle("SuperADD")]
+[assembly: AssemblyDescription("Super MDT Active Directory Deploy")]
+[assembly: AssemblyConfiguration("Dylan Bickerstaff")]
+[assembly: AssemblyCompany("Dylan Bickerstaff")]
+[assembly: AssemblyProduct("SuperADD")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Dylan Bickerstaff")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
